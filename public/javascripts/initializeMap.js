4609864fedebcf69a8899155a106e5dbc7bee2d1
Shawn Li <shawnli2010@126.com>
2018-04-07 22:57:09 -0400

    implemented click on cell will highlight cell and marker; implemented scroll to certain cell when click on marker or cell

--- public/javascripts/initializeMap.js
+++ public/javascripts/initializeMap.js
@@ -1,7 +1,6 @@
 var map;
 var eventIdToMarker = {};
 var eventIdToMarkerImagePath = {};
-// var markerToEventId = {};
 
 var colorMap = {
   'black' : '#000000',
@@ -17,16 +16,34 @@ var colorNamesArray = Object.keys(colorMap);
 
 var selectedMarkerImagePath = 'images/map-marker-selected.png';
 
-function unselectMarkerAndCell(eventIdToMarker, eventIdToMarkerImagePath) {
+// 1. Get the selected eventId from localStorage
+// 2. un-highlight event's marker and event's cell
+function unselectMarkerAndCell() {
   var oldSelectedEventId = localStorage.getItem('selectedEventId');
+  
   if (oldSelectedEventId != null) {
     var oldSelectedMarker = eventIdToMarker[oldSelectedEventId];
     var oldSelectedMarkerImagePath = eventIdToMarkerImagePath[oldSelectedEventId];
     oldSelectedMarker.setIcon(oldSelectedMarkerImagePath);
 
-    var oldSelectedEventCellCSSId = '#cell_' + oldSelectedEventId;
-    $(oldSelectedEventCellCSSId).css('background', '#FFFFFF');
+    var oldSelectedCell = $('#cell_' + oldSelectedEventId);
+    oldSelectedCell.css('background', '#FFFFFF');
   }
+};
+
+// 1. Get the marker from the eventIdToMarker map
+// 2. Highlight event's marker and event's cell
+function selectMarkerAndCell(eventId) {
+  var marker = eventIdToMarker[eventId];
+
+  marker.setIcon(selectedMarkerImagePath);
+
+  var table = $('.list-group');
+  var selectedCell = $('#cell_' + eventId);
+  selectedCell.css('background', colorMap['pink']);
+  scrollToCertainCell(table, selectedCell);
+
+  localStorage.setItem('selectedEventId', eventId);
 };
 
 function assignColorToColorIndicator(colorName, eventId, markerImagePath) {
@@ -34,6 +51,15 @@ function assignColorToColorIndicator(colorName, eventId, markerImagePath) {
   var colorIndicatorCSSId = '#indicator_' + eventId;
   $(colorIndicatorCSSId).css('background', colorIndicatorHashvalue);    
 }
+
+// Helper method from:
+// https://stackoverflow.com/questions/2905867/how-to-scroll-to-specific-item-using-jquery?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+// NEED TO FIGURE OUT WHY IT WORKS
+function scrollToCertainCell(container, targetCell) {
+  container.animate({
+      scrollTop: targetCell.offset().top - container.offset().top + container.scrollTop()
+  });
+};
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('allmap'), {
@@ -45,7 +71,9 @@ function initMap() {
       localStorage.removeItem('selectedEventId');
       
       for (var i in data) {
+        (function(){
         var eventId = data[i]._id;
+        var eventName = data[i].name;
         var latitude = data[i].areaCoordinates[1];
         var longitude = data[i].areaCoordinates[0];
         var myLatLng = {lat: latitude, lng: longitude};
@@ -57,33 +85,31 @@ function initMap() {
         assignColorToColorIndicator(colorName, eventId, markerImagePath);
         eventIdToMarkerImagePath[eventId] = markerImagePath;
 
-        // marker.title stores the id of the corresponding event (temp)
         var marker = new google.maps.Marker({
           position: myLatLng,
           map: map,
-          title: eventId,
+          title: eventName,
           icon: markerImagePath
         });
         eventIdToMarker[eventId] = marker;
 
         marker.addListener('click', function() {
           // console.log('marker clicked');
-          unselectMarkerAndCell(eventIdToMarker, eventIdToMarkerImagePath);
-
-          // marker.title stores the id of the corresponding event (temp)
-          var thisEventId = this.title;      
-          this.setIcon(selectedMarkerImagePath);
-          var selectedEventCellCSSId = '#cell_' + thisEventId;
-          $(selectedEventCellCSSId).css('background', colorMap['pink']);
-
-          // marker.title stores the id of the corresponding event (temp)
-          localStorage.setItem('selectedEventId', this.title);
+          unselectMarkerAndCell();
+          selectMarkerAndCell(eventId);          
         });
+
+        var selectedCell = $('#cell_' + eventId);
+        selectedCell.click(function(){          
+          unselectMarkerAndCell();
+          selectMarkerAndCell(eventId);          
+        });
+       }());
       }
 
       map.addListener('click', function() {
         // console.log('map clicked');
-        unselectMarkerAndCell(eventIdToMarker, eventIdToMarkerImagePath);
+        unselectMarkerAndCell();
       });          
   });
 };
